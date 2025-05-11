5edc87b0c9f72ef1f85c917bf6d57251d4270c52
Documenter.jl <documenter@juliadocs.github.io>
2025-05-11 15:35:17 +0000

    build based on 3b6e14a

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#DAECompiler","page":"Home","title":"DAECompiler","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Getting started with DAECompiler.","category":"page"}]
+[{"page":"Home","location":"#DAECompiler","title":"DAECompiler","category":"section","text":""},{"page":"Home","location":"","title":"Home","category":"page","text":"Getting started with DAECompiler."}]
 }
