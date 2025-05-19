fce94c1e1904b2e5ac5dfe76a2cf6ed865070e28
Keno Fischer <keno@juliacomputing.com>
2025-05-19 15:12:03 -0400

    Revive system consistency checking (#38)
    * Revive system consistency checking
    
    I thought the benchmark was unbalanced, but it's not, I just forgot that some equations were `initial!`.
    Still, at least it gave me a reason to put back the consistency checking.
    
    * rm debug print
    
    * Comment out scope-dependent test

---
+++ test/errors.jl
@@ -0,0 +1,30 @@
+module Basic
+
+using Test
+using DAECompiler
+using DAECompiler.Intrinsics
+using Sundials
+using SciMLBase
+using OrdinaryDiffEq
+
+function unblanaced_too_many_eqs()
+    x = continuous(#= :x =#)
+    always!(x, #= :foo=#)
+    always!(x^2 #= Scope(Scope(Scope(), :Bar), :qux) =#)
+end
+@test_throws ["The system is unbalanced.",
+    "There are 1 highest order differentiated variable(s) and 2 equation(s).",
+    #= r"(▫.Bar.qux)|(▫.foo)", =#
+    "was potentially redundant:"] solve(ODECProblem(unblanaced_too_many_eqs, (1,) .=> 1.), Rodas5(autodiff=false))
+
+function structurally_singular()
+    x = continuous(#= :x =#)
+    u1 = continuous(#= :u1 =#)
+    u2 = continuous(#= :u2 =#)
+    always!.((x + sin(u1 + u2),
+        ddt(x) - (x + 1.0),
+        cos(x) - 2.0))
+end
+@test_throws "The system is structurally singular.\nThis variable may be problematic:" solve(ODECProblem(structurally_singular, (1,) .=> 1.), Rodas5(autodiff=false))
+
+end
