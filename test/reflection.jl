0d65bb71f4d6842fca54e642c0d4905624999a38
Cédric Belmant <cedric.belmant@juliahub.com>
2025-06-02 17:00:11 +0000

    Add test

--- test/reflection.jl
+++ test/reflection.jl
@@ -5,6 +5,8 @@ using DAECompiler
 using DAECompiler.Intrinsics
 using Compiler: IRCode
 
+@noinline callee!(x, y) = always!(x + y)
+
 function ssrm()
     x₁ = continuous()
     x₂ = continuous()
@@ -20,7 +22,7 @@ function ssrm()
     always!((ẍ₁+ẍ₂)-(ẋ₁+ẋ₂)+x₄)
     always!((ẍ₁+ẍ₂)+x₃)
     always!(x₂+ẍ₃+ẋ₄)
-    always!(x₃+ẋ₄)
+    callee!(x₃,ẋ₄)
 end
 
 ir = code_structure_by_type(Tuple{typeof(ssrm)})
@@ -33,5 +35,8 @@ ir = @code_structure world = Base.get_world_counter() ssrm()
 @test isa(ir, IRCode)
 result = @code_structure result = true ssrm()
 @test isa(result, DAECompiler.DAEIPOResult)
+result = @code_structure matched = true ssrm()
+@test isa(result, DAECompiler.MatchedSystemStructure)
+@test contains(sprint(show, result), r"%\d+")
 
 end # module
